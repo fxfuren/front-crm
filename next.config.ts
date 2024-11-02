9bebc4d9fee66f755885d877109e4ce51006875b
FxFurEN <nikitostimoshenko@gmail.com>
2024-11-02 03:01:55 +0300

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// Экспериментальные функции для Next.js
+	experimental: {
+		// Отключение функции "missing Suspense with CSR bailout"
+		missingSuspenseWithCSRBailout: false
+	},
+	env: {
+		// URL сервера для API-запросов, получаемый из переменных окружения
+		SERVER_URL: process.env.SERVER_URL,
+		// Ключ сайта Google reCAPTCHA для клиентской валидации
+		GOOGLE_RECAPTCHA_SITE_KEY: process.env.GOOGLE_RECAPTCHA_SITE_KEY
+	},
 };
 
 export default nextConfig;
