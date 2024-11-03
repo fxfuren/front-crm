5e4af31c4b1cec91c35fee4e65341124bc39ae11
FxFurEN <nikitostimoshenko@gmail.com>
2024-11-03 03:22:07 +0300

    add auth forms

---
+++ src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,117 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useTheme } from 'next-themes'
+import Link from 'next/link'
+import { useState } from 'react'
+import ReCAPTCHA from 'react-google-recaptcha'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+import {
+	Button,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+	Input
+} from '@/shared/components/ui'
+
+import { LoginSchema, type TypeLoginSchema } from '../schemes'
+
+import { AuthWrapper } from './index'
+
+/**
+ * Форма для входа в систему.
+ */
+export function LoginForm() {
+	const { theme } = useTheme()
+	const [recaptchaValue, setRecaptchaValue] = useState<string | null>(null)
+
+	const form = useForm<TypeLoginSchema>({
+		resolver: zodResolver(LoginSchema),
+		defaultValues: {
+			email: '',
+			password: ''
+		}
+	})
+
+	const onSubmit = (values: TypeLoginSchema) => {
+		if (recaptchaValue) {
+			// login({ values, recaptcha: recaptchaValue })
+		} else {
+			toast.error('Пожалуйста, завершите reCAPTCHA')
+		}
+	}
+
+	return (
+		<AuthWrapper
+			heading='Войти'
+			description='Чтобы войти на сайт введите ваш email и пароль'
+			backButtonLabel='Еще нет аккаунта? Регистрация'
+			backButtonHref='/auth/register'
+		>
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className='grid gap-2 space-y-2'
+				>
+					<FormField
+						control={form.control}
+						name='email'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Почта</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='ivan@example.com'
+										type='email'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='password'
+						render={({ field }) => (
+							<FormItem>
+								<div className='flex items-center justify-between'>
+									<FormLabel>Пароль</FormLabel>
+									<Link
+										href='/auth/reset-password'
+										className='ml-auto inline-block text-sm underline'
+									>
+										Забыли пароль?
+									</Link>
+								</div>
+								<FormControl>
+									<Input
+										placeholder='******'
+										type='password'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<div className='flex justify-center'>
+						<ReCAPTCHA
+							sitekey={
+								process.env.GOOGLE_RECAPTCHA_SITE_KEY as string
+							}
+							onChange={setRecaptchaValue}
+							theme={theme === 'light' ? 'light' : 'dark'}
+						/>
+					</div>
+					<Button type='submit'>Войти в аккаунт</Button>
+				</form>
+			</Form>
+		</AuthWrapper>
+	)
+}
---
+++ src/features/auth/components/RegisterForm.tsx
@@ -0,0 +1,140 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useTheme } from 'next-themes'
+import { useState } from 'react'
+import ReCAPTCHA from 'react-google-recaptcha'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+import {
+	Button,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+	Input
+} from '@/shared/components/ui'
+
+import { RegisterSchema, type TypeRegisterSchema } from '../schemes'
+
+import { AuthWrapper } from './index'
+
+/**
+ * Форма регистрации нового пользователя.
+ */
+export function RegisterForm() {
+	const { theme } = useTheme()
+	const [recaptchaValue, setRecaptchaValue] = useState<string | null>(null)
+
+	const form = useForm<TypeRegisterSchema>({
+		resolver: zodResolver(RegisterSchema),
+		defaultValues: {
+			name: '',
+			email: '',
+			password: '',
+			passwordRepeat: ''
+		}
+	})
+
+	const onSubmit = (values: TypeRegisterSchema) => {
+		if (recaptchaValue) {
+			// register({ values, recaptcha: recaptchaValue })
+		} else {
+			toast.error('Пожалуйста, завершите reCAPTCHA')
+		}
+	}
+
+	return (
+		<AuthWrapper
+			heading='Регистрация'
+			description='Чтобы войти на сайт введите ваш email и пароль'
+			backButtonLabel='Уже есть аккаунт? Войти'
+			backButtonHref='/auth/login'
+		>
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className='grid gap-2 space-y-2'
+				>
+					<FormField
+						control={form.control}
+						name='name'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Имя</FormLabel>
+								<FormControl>
+									<Input placeholder='Иван' {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='email'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Почта</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='ivan@example.com'
+										type='email'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='password'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Пароль</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='******'
+										type='password'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name='passwordRepeat'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Повторите пароль</FormLabel>
+								<FormControl>
+									<Input
+										placeholder='******'
+										type='password'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<div className='flex justify-center'>
+						<ReCAPTCHA
+							sitekey={
+								process.env.GOOGLE_RECAPTCHA_SITE_KEY as string
+							}
+							onChange={setRecaptchaValue}
+							theme={theme === 'light' ? 'light' : 'dark'}
+						/>
+					</div>
+					<Button type='submit'>Создать аккаунт</Button>
+				</form>
+			</Form>
+		</AuthWrapper>
+	)
+}
