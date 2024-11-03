b2ff2c7b964841ee89d67ec09adde3e52af0b074
FxFurEN <nikitostimoshenko@gmail.com>
2024-11-03 19:23:12 +0300

    view settings

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,3 +1,4 @@
+import { SettingsForm } from '@/features/user/components'
 import { buttonVariants } from '@/shared/components/ui'
 import { MoveLeft } from 'lucide-react'
 import Link from 'next/link'
@@ -6,11 +7,11 @@ import Link from 'next/link'
 export default function SettingsPage() {
 	return (
 		<div className='space-y-5 text-center'>
-			<h1 className='text-4xl font-bold'>Setttigs</h1>
 			<Link href='/dashboard' className={buttonVariants()}>
 				<MoveLeft />
 				Dashboard
 			</Link>
+			<SettingsForm />
 		</div>
 	)
 }
