54813649989016514afdd36e381eb14bed594a33
FxFurEN <nikitostimoshenko@gmail.com>
2024-11-03 19:22:48 +0300

    add ui  components

--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -1,3 +1,4 @@
+export * from './Avatar'
 export * from './button'
 export * from './Card'
 export * from './dropdown-menu'
@@ -5,5 +6,6 @@ export * from './Form'
 export * from './Input'
 export * from './Label'
 export * from './Loading'
+export * from './Skeleton'
 export * from './Sonner'
 
