a3db0670493644e4e41ebb2d462641525f081d79
nikita <nikitostimoshenko@gmail.com>
2024-11-10 03:05:16 +0300

    add new api handlers

--- src/features/user/services/user.service.ts
+++ src/features/user/services/user.service.ts
@@ -29,6 +29,27 @@ class UserService {
 
 		return response
 	}
+
+	/**
+	 * Генерирует ссылку приглашения для регистрации.
+	 *
+	 * @returns {Promise<IUser>} - Обновленный профиль пользователя.
+	 */
+	public async generateInviteToken() {
+		const response = await api.post<IUser>('users/new')
+
+		return response
+	}
+
+	/**
+	 * Получает все инвайт токены текущего пользователя.
+	 *
+	 * @returns {Promise<any[]>} - Массив инвайт токенов.
+	 */
+	public async getInviteTokens() {
+		const response = await api.get<any[]>('users/invite-tokens')
+		return response
+	}
 }
 
 export const userService = new UserService()
