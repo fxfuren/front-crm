bcb248c62af1dcd8b124faafcde163bc55361377
nikita <nikitostimoshenko@gmail.com>
2024-11-15 14:03:45 +0300

    integration

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import { DataTable } from '@/features/dashboard/components/data-table/data-table'
-import { columns } from '@/features/orders/components'
+import { orderColumns } from '@/features/orders/components'
 import { IOrder, OrderStatus } from '@/features/orders/types'
 import { UserRole } from '@/features/user/types'
 
 const orders: IOrder[] = [
 	{
-		id: 'ORDER-1234',
+		id: 'ORDER-14',
 		customer: 'Иван Иванов',
 		device: 'iPhone 12',
 		issue: 'Не включается',
@@ -22,7 +22,27 @@ const orders: IOrder[] = [
 			updatedAt: new Date().toISOString()
 		},
 		technicianId: 'TECH-1',
-		status: OrderStatus.IN_PROGRESS,
+		status: OrderStatus.InProgress,
+		createdAt: new Date(),
+		completedAt: null
+	},
+	{
+		id: 'ORDER-15',
+		customer: 'Вадим Смирнов',
+		device: 'iPhone 12 pro max',
+		issue: 'Замена батареии',
+		technician: {
+			id: 'TECH-1',
+			email: 'technician@example.com',
+			password: 'password123',
+			displayName: 'Техник A',
+			role: UserRole.Admin,
+			isVerified: true,
+			createdAt: new Date().toISOString(),
+			updatedAt: new Date().toISOString()
+		},
+		technicianId: 'TECH-1',
+		status: OrderStatus.InProgress,
 		createdAt: new Date(),
 		completedAt: null
 	}
@@ -31,7 +51,7 @@ const orders: IOrder[] = [
 export default function OrdersPage() {
 	return (
 		<div className='h-full flex-1 flex-col space-y-8 p-8 md:ml-0 md:flex'>
-			<DataTable data={orders} columns={columns} />
+			<DataTable data={orders} columns={orderColumns} />
 		</div>
 	)
 }
