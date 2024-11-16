033ff809f8f66519cba53b2d5b9f65c85d699e9f
nikita <nikitostimoshenko@gmail.com>
2024-11-16 03:40:06 +0300

    fix view

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -18,13 +18,13 @@ export default function DashboardLayout({
 	return (
 		<SidebarProvider>
 			<DasboardSidebar />
-			<SidebarInset>
+			<SidebarInset className='overflow-hidden'>
 				<header className='flex h-16 shrink-0 items-center gap-2 px-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12'>
 					<SidebarTrigger className='-ml-1' />
 					<Separator orientation='vertical' className='mr-2 h-4' />
 					<BreadcrumbNavbar />
 				</header>
-				<div className='flex flex-1 flex-col items-center justify-center gap-4 p-4 px-4 pt-0'>
+				<div className='mx-auto flex w-full flex-1 flex-col md:p-6'>
 					{children}
 				</div>
 			</SidebarInset>
