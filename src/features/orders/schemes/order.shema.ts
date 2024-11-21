9a668f23da8e72789829e39b222e0c3a3e8e1559
nikita <nikitostimoshenko@gmail.com>
2024-11-21 23:37:49 +0300

    add schema

---
+++ src/features/orders/schemes/order.shema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod'
+
+export const formSchema = z.object({
+	customer: z.string().min(2, {
+		message: 'Имя клиента должно быть не менее 2 символов.'
+	}),
+	device: z.string().min(2, {
+		message: 'Устройство должно быть не менее 2 символов.'
+	}),
+	issue: z.string().min(5, {
+		message: 'Описание проблемы должно быть не менее 5 символов.'
+	})
+})
