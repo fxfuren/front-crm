953e6232de08b198dad3a06ec858258a142679a2
nikita <nikitostimoshenko@gmail.com>
2024-11-21 23:37:40 +0300

    add order service

--- src/features/orders/services/orders.service.ts
+++ src/features/orders/services/orders.service.ts
@@ -15,6 +15,17 @@ class OrderService {
 		const response = await api.get<IOrder>('orders')
 		return response
 	}
+
+	/**
+	 * Добавление нового заказа.
+	 *
+	 * @param {Partial<IOrder>} body - Данные нового заказа.
+	 * @returns {Promise<IOrder>} - Ответ с данными созданного заказа.
+	 */
+	public async addOrder(body: Partial<IOrder>) {
+		const response = await api.post<IOrder>('orders/new', body)
+		return response
+	}
 }
 
 export const ordersService = new OrderService()
