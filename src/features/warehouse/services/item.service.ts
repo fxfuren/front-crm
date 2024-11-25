bc8a4ee62dca2671ba963782763ed639dea62eb0
nikita <nikitostimoshenko@gmail.com>
2024-11-26 00:27:57 +0300

    add services

--- src/features/warehouse/services/item.service.ts
+++ src/features/warehouse/services/item.service.ts
@@ -15,6 +15,27 @@ class ItemWarehoseService {
 		const response = await api.get<IItem>('items')
 		return response
 	}
+
+	/**
+	 * Добавление нового заказа.
+	 *
+	 * @param {Partial<IItem>} body - Данные нового товара.
+	 * @returns {Promise<IItem>} - Ответ с данными созданного товара.
+	 */
+	public async addItem(body: Partial<IItem>) {
+		const response = await api.post<IItem>('items/new', body)
+		return response
+	}
+
+	/**
+	 * Удаление товара со склада.
+	 *
+	 * @param {string} itemId - Идентификатор товара для удаления.
+	 * @returns {Promise<void>} - Пустой ответ при успешном удалении.
+	 */
+	public async deleteItem(itemId: string) {
+		await api.delete(`items/${itemId}`)
+	}
 }
 
 export const itemWarehoseService = new ItemWarehoseService()
