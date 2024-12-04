ea528462c891e781777cb995e4289463a85eb682
nikita <nikitostimoshenko@gmail.com>
2024-12-04 05:55:33 +0300

    Simplified description validation in warehouse item schema

--- src/features/warehouse/schemes/warehouse.item.shema.ts
+++ src/features/warehouse/schemes/warehouse.item.shema.ts
@@ -7,12 +7,7 @@ export const formSchema = z.object({
 	quantity: z.number().min(1, {
 		message: 'Количество должно быть не менее 1.'
 	}),
-	description: z
-		.string()
-		.min(5, {
-			message: 'Описание проблемы должно быть не менее 5 символов.'
-		})
-		.optional(),
+	description: z.string().optional(),
 	price: z.string().refine(value => /^\d+(\.\d{1,2})?$/.test(value), {
 		message: 'Цена должна быть числом с не более чем 2 десятичными знаками.'
 	})
